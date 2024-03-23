d5afe5145fa726d3564b2aa142c28d4a30a2bdd3
thownef <vantho3042000@gmail.om>
2024-03-23 14:51:26 +0700

    reset code

--- config/connect.js
+++ config/connect.js
@@ -1,14 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("vjc", "root", null, {
-  host: "localhost",
+const sequelize = new Sequelize("railway", "root", "LPlqlnbwlNZWZVagUJfLRlVFornlHESF", {
+  host: "roundhouse.proxy.rlwy.net",
+  port: 37820,
   dialect: "mysql",
 });
 
 const connection = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    console.log("Connect db successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 import express from "express";
-import cors from "cors";
-import multer from "multer";
-import { S3Client } from "@aws-sdk/client-s3";
-import multerS3 from "multer-s3";
 import dotenv from "dotenv";
-import serverless from "serverless-http";
-import { generateUploadURL } from "./s3.js";
-import EmailSender from "./sendEmail.js";
-import EmailSenderExpert from "./sendMailExpert.js";
-import EmailSenderUser from "./sendEmailNoti.js";
-import connection from "./config/connect.js";
+import cors from "cors";
+import connection from "./config/connect";
 
 dotenv.config();
 
@@ -18,104 +10,6 @@ app.use(express.json());
 app.use(cors());
 connection()
 
-const region = "ap-southeast-1";
-const bucketName = "vjpconnect";
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-
-const s3 = new S3Client({
-  region,
-  accessKeyId,
-  secretAccessKey,
-});
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: bucketName,
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  }),
-});
-
-app.post("/upload", upload.single("photos"), function (req, res) {
-  console.log(req.file);
-  res.send("Successfully uploaded");
-});
-
-app.get("/s3Url", async (req, res) => {
-  const url = await generateUploadURL();
-  res.send({ url });
-});
-
-app.post("/server/send", async (req, res) => {
-  try {
-    const {
-      fullname,
-      vocative,
-      company_name,
-      company_contact,
-      email,
-      email_contact,
-      phone,
-      description,
-      isContact,
-    } = req.body;
-    EmailSender({
-      fullname,
-      vocative,
-      company_name,
-      company_contact,
-      email,
-      email_contact,
-      phone,
-      description,
-      isContact,
-    });
-    res.status(200).json({ message: "Your message sent successfully" });
-  } catch (error) {
-    res.status(404).json({ message: "Error ❌" });
-  }
-});
-
-app.post("/server/sendExpert", async (req, res) => {
-  try {
-    const { fullname, expert_name, company_name, email, phone, content } =
-      req.body;
-    EmailSenderExpert({
-      fullname,
-      expert_name,
-      company_name,
-      email,
-      phone,
-      content,
-    });
-    res.status(200).json({ message: "Your message sent successfully" });
-  } catch (error) {
-    res.status(404).json({ message: "Error ❌" });
-  }
-});
-
-app.post("/server/send-user", async (req, res) => {
-  try {
-    const { fullname, company_name, email, phone } = req.body;
-    EmailSenderUser({
-      fullname,
-      company_name,
-      email,
-      phone,
-    });
-    res.json({ msg: "Your message sent successfully" });
-  } catch (error) {
-    res.status(404).json({ msg: "Error ❌" });
-  }
-});
-
-
-app.listen(8080, () => {
-  console.log("Connected to backend!");
-});
-
-export default serverless(app);
+app.listen(8888, () => {
+  console.log("Connected to backend")
+})
